c28fa2feccf2bf6477b04022633c3b0c8b6320cb
Lucien Chemaly <chemaly.lucien@gmail.com>
2023-07-31 15:39:38 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# How to GET jobs data using Merge's Unified API 
+
+This is a sample repository on how to get Jobs data from BambooHR using Merge's Unified API
+
+## Install the required libraries
+```
+pip3 install pandas
+pip3 install merge-ats-client
+```
+
+### Run the code
+
+```
+python3 bamboohr_jobs.py
+python3 bamboohr_jobs_open.py
+```
+
+> Note : Replace 'YOUR_MERGE_API_KEY' and 'YOUR_MERGE_LINKED_ACCOUNT_KEY' with your actual Merge API key and linked account key, respectively.
---
+++ bamboohr_jobs.py
@@ -0,0 +1,39 @@
+import MergeATSClient
+from MergeATSClient.api import jobs_api
+import csv
+
+configuration = MergeATSClient.Configuration(
+    host="https://api.merge.dev/api/ats/v1"
+)
+
+configuration.api_key['tokenAuth'] = 'YOUR_MERGE_API_KEY'
+configuration.api_key_prefix['tokenAuth'] = 'Bearer'
+
+with MergeATSClient.ApiClient(configuration) as api_client:
+    api_instance = jobs_api.JobsApi(api_client)
+    x_account_token = 'YOUR_MERGE_LINKED_ACCOUNT_KEY'
+
+    try:
+        api_response = api_instance.jobs_list(x_account_token)
+
+        # Define the CSV file name
+        csv_file = "bamboohr_jobs_results.csv"
+
+        # Write the API response to a CSV file
+        with open(csv_file, mode='w', newline='') as file:
+            writer = csv.writer(file)
+
+            # Write header row
+            writer.writerow(["Name", "Status", "Created At"])
+
+            # Write data rows
+            for job in api_response["results"]:
+                name = job["name"]
+                status = job["status"]
+                created_at = job["remote_created_at"]
+
+                writer.writerow([name, status, created_at])
+
+        print(f"Results written to {csv_file}")
+    except MergeATSClient.ApiException as e:
+        print('Exception when calling JobsApi->jobs_list: %s' % e)
